b0acdc457dadebdc08bbbb31eb70d0fb0dfde904
SyedShamail1 <syedshamail3@gmail.com>
2023-04-27 21:29:10 +0500

    initial commit

---
+++ WebAPI/Data/ContactsApiDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebAPI.Models;
+namespace WebAPI.Data
+
+{
+    public class ContactsApiDbContext : DbContext
+    {
+        public ContactsApiDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Contact> Contacts { get; set; }
+    }
+}
---
+++ WebAPI/Models/AddContactRequest.cs
@@ -0,0 +1,14 @@
+﻿namespace WebAPI.Models
+
+{
+    public class AddContactRequest
+    {
+        public string FullName { get; set; }
+
+        public string Email { get; set; }
+
+        public long Phone { get; set; }
+
+        public string Address { get; set; }
+    }
+}
